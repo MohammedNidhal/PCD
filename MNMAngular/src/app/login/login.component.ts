cc4fa01f5433cf94395ad2c9bffcde6f982bfc8c
Mohammed Nidhal Nasri <mohammednidhal2001@gmail.com>
2024-04-01 15:08:58 +0100

    Added Html/TS for the login/signup components it still needs backend

--- MNMAngular/src/app/login/login.component.ts
+++ MNMAngular/src/app/login/login.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from './auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string;
+  password: string;
+
+  constructor(private authService: AuthService) { }
+
+  login() {
+    this.authService.login(this.username, this.password)
+      .subscribe(
+        data => {
+          // Handle successful login
+        },
+        error => {
+          // Handle login error
+        }
+      );
+  }
 
 }
--- MNMAngular/src/app/signup/signup.component.ts
+++ MNMAngular/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SignupService } from './signup.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  step: number = 1;
+  userData: any = {};
+  medicalData: any = {};
 
+  constructor(private signupService: SignupService) { }
+
+  nextStep() {
+    this.step++;
+  }
+
+  submit() {
+    const userDataAndMedicalData = { ...this.userData, ...this.medicalData };
+    this.signupService.signup(userDataAndMedicalData)
+      .subscribe(
+        data => {
+          // Handle successful signup
+        },
+        error => {
+          // Handle signup error
+        }
+      );
+  }
 }
