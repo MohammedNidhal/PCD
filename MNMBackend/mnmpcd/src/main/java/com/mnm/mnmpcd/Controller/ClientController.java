d777fdfec106dc1156eeeb93d0d66e634cc00e53
Mohammed Nidhal Nasri <mohammednidhal2001@gmail.com>
2024-04-03 14:40:04 +0100

    Adding session managment

--- MNMBackend/mnmpcd/src/main/java/com/mnm/mnmpcd/Controller/ClientController.java
+++ MNMBackend/mnmpcd/src/main/java/com/mnm/mnmpcd/Controller/ClientController.java
@@ -1,32 +1,41 @@
 package com.mnm.mnmpcd.Controller;
-import org.springframework.web.bind.annotation.RestController;
-import com.mnm.mnmpcd.ClientDAO;
+
+import com.mnm.mnmpcd.DAO.ClientDAO; 
+import com.mnm.mnmpcd.Response.LoginResponse; 
 import com.mnm.mnmpcd.Services.ClientService;
-import corg.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpSession;
+
 @RestController
 @CrossOrigin
 @RequestMapping("api/v1/client")
-
 public class ClientController {
+
     @Autowired
     private ClientService clientService;
 
     @PostMapping(path="/save")
-    public String saveClient(@RequestBody ClientDAO clientDAO){
-    String id = clientService.addClient(clientDAO);
-    return(id) ;
+    public String saveClient(@RequestBody ClientDAO clientDAO) {
+        String id = clientService.addClient(clientDAO);
+        return id; // Removed unnecessary parenthesis
     }
 
     @PostMapping(path="/login")
-    public ResponseEntity<?> loginClient(@RequestBody LoginDAO loginDAO){
-    LoginResponse loginResponse = clientService.loginClient(loginDAO);
-    return(ResponseEntity.ok(loginResponse)) ;
+    public ResponseEntity<LoginResponse> loginClient(@RequestBody LoginDAO loginDAO, HttpSession session) {
+        LoginResponse loginResponse = clientService.loginClient(loginDAO,session);
+        return ResponseEntity.ok(loginResponse); 
     }
-
-
-
-
-
-
-
+    @PostMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "Logout successful";
+    }
 }
--- MNMBackend/mnmpcd/src/main/java/com/mnm/mnmpcd/Services/ClientService.java
+++ MNMBackend/mnmpcd/src/main/java/com/mnm/mnmpcd/Services/ClientService.java
@@ -2,10 +2,11 @@ package com.mnm.mnmpcd.Services;
 import com.mnm.mnmpcd.DAO.ClientDAO; 
 import com.mnm.mnmpcd.DAO.LoginDAO; 
 import com.mnm.mnmpcd.Response.LoginResponse;
+import javax.servlet.http.HttpSession;
 
 public interface ClientService {
 
 String addClient(ClientDAO clientDAO) ;
-LoginResponse loginClient(LoginDAO loginDAO) ;
+LoginResponse loginClient(LoginDAO loginDAO, HttpSession session) ;
 
 }
