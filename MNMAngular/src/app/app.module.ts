721d93597b7d3c70e6c01ce73abe4ce7a1488135
Mohammed Nidhal Nasri <mohammednidhal2001@gmail.com>
2024-04-03 15:13:47 +0100

    Dashboard

--- MNMAngular/src/app/app.module.ts
+++ MNMAngular/src/app/app.module.ts
@@ -6,12 +6,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
+import { MedicalDataComponent } from './medical-data/medical-data.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HeaderComponent,
+    SidenavComponent,
+    DashboardComponent,
+    OrderHistoryComponent,
+    MedicalDataComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
