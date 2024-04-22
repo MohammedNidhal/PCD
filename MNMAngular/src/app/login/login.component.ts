4c11751e27c7981dde15ba5495932d667a8ed117
Mohammed Nidhal Nasri <mohammednidhal2001@gmail.com>
2024-04-22 13:46:45 +0100

    Backend updated

--- MNMAngular/src/app/login/login.component.ts
+++ MNMAngular/src/app/login/login.component.ts
@@ -1,26 +1,28 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  username!: string;
-  password!: string;
+  email: string = '';
+  password: string = '';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private loginService: LoginService, private router: Router) { }
 
   login() {
-    this.authService.login(this.username, this.password)
+    this.loginService.getClientDetails(this.email, this.password)
       .subscribe(
         data => {
-          // Handle successful login
+          this.router.navigate(['/Dashboard']);
         },
         error => {
           // Handle login error
         }
       );
   }
-
 }
